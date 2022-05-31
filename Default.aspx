29c6017d1820bae5328ed70dd8ff36e3459d8ac4
kwonmink <minkung3234@naver.com>
2022-05-31 14:29:24 +0900

    commit

---
+++ Default.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true"  CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>無題のページ</title>
+</head>
+<body>
+    <form id="form1" runat="server">   
+      
+        <asp:Button ID="Button1" runat="server" Text="検索" onclick="Button1_Click" />&nbsp;<br />
+        <asp:TextBox ID="TextBox1" runat="server"/><br />
+        <asp:Label ID="Label1" runat="server" Text=""/><br /><br />
+        
+        <asp:Table ID="Table1" runat="server" BorderColor="#3333CC" BorderWidth="1px" GridLines="Both">
+        </asp:Table>
+        
+       
+
+        
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text;
+
+public partial class _Default : System.Web.UI.Page 
+{
+
+    public SqlConnection db_Connect()
+    {
+        return new SqlConnection(ConfigurationManager.ConnectionStrings["ApplicationServices"].ConnectionString);
+    }
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+       
+
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        TableRow tableRow;
+        TableCell tableCell;
+
+        tableRow = new TableRow();
+
+        tableCell = new TableCell();
+        tableCell.Text = "employeeid";
+        tableRow.Cells.Add(tableCell);
+
+        tableCell = new TableCell();
+        tableCell.Text = "employname";
+        tableRow.Cells.Add(tableCell);
+
+
+        Table1.Rows.Add(tableRow);
+
+        String InputStr1;
+        String strdEmployname;
+
+        InputStr1 = TextBox1.Text;
+        Label1.Text = InputStr1 + " が入力されました。";
+
+        string connString = ConfigurationManager.ConnectionStrings["test"].ConnectionString;
+        string queryString = "SELECT employeeid, employname FROM employee WHERE employname=" + "'" + InputStr1 + "'";
+
+        using (SqlConnection conn = new SqlConnection(connString))
+        {
+            SqlCommand comm = new SqlCommand(queryString, conn);
+            conn.Open();
+
+            using (SqlDataReader dr = comm.ExecuteReader())
+            {
+                while (dr.Read())
+                {
+                    strdEmployname = dataread(1).ToString;
+
+                    //TABLE生成
+                    tableRow = new TableRow();
+
+                    tableCell = new TableCell();
+                    tableCell.text = dataread(0).ToString;
+                    tableRow.Cells.Add(tableCell);
+
+                    tableCell = new TableCell();
+                    tableCell.text = dataread(1).ToString;
+                    tableRow.Cells.Add(tableCell);
+
+                    Table1.Rows.Add(tableRow);
+                }
+            }
+        }
+
+       
+
+      
+
+    }
+}
